41296a180b41c3da3b8dd9a077d9622d8c36e4b2
bmariem <mariem.borchani@gmail.com>
2022-02-16 20:40:12 +0100

    Fixe route comic

--- config/api-axios.js
+++ config/api-axios.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const instance = axios.create({
-  baseURL: "https://lereacteur-marvel-api.herokuapp.com/", // API Marvel
+  baseURL: "https://lereacteur-marvel-api.herokuapp.com", // API Marvel
 });
 
 module.exports = instance;
--- routes/comics.js
+++ routes/comics.js
@@ -6,14 +6,20 @@ const axios = require("../config/api-axios");
 router.get("/comics", async (req, res) => {
   try {
     console.log("Route: /comics");
-    const skip = (req.query.page - 1) * 100; // limit <=> between 1 and 100
-    // search a comic by title <=> req.query.search
-    console.log(req.query.page);
-    console.log(skip);
 
-    const response = await axios.get(
-      `/comics?apiKey=${process.env.API_SECRET_MARVEL}&skip=${skip}&title=${req.query.search}`
-    );
+    let url = `/comics?apiKey=${process.env.API_SECRET_MARVEL}`;
+
+    if (req.query.search) {
+      // search a comic by title <=> req.query.search
+      url = `${url}&title=${req.query.search}`;
+    }
+
+    if (req.query.page) {
+      const skip = (req.query.page - 1) * 100; // limit <=> between 1 and 100
+      url = `${url}&skip=${skip}`;
+    }
+
+    const response = await axios.get(url);
     console.log(response.data);
 
     res.status(200).json(response.data);
@@ -23,16 +29,16 @@ router.get("/comics", async (req, res) => {
   }
 });
 
-// Get a list of comics containing a specific character
-router.get("/comics/:id", async (req, res) => {
+// Get a comics by id
+router.get("/comic/:id", async (req, res) => {
   try {
-    console.log("Route: /comics/:id");
+    console.log("Route: /comic/:id");
 
     // search a comic by id
     console.log(req.params.id);
 
     const response = await axios.get(
-      `/comics/${req.params.id}?apiKey=${process.env.API_SECRET_MARVEL}`
+      `/comic/${req.params.id}?apiKey=${process.env.API_SECRET_MARVEL}`
     );
 
     res.status(200).json(response.data);
