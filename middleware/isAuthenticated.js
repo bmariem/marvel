19580cb4e53956921bd2d35401b97cbc699cdb6b
bmariem <mariem.belgacem@toucantoco.com>
2022-02-23 16:46:13 +0100

    add comments to isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 
+// only an authenticated user will be able to access the route where isAuthenticated is called
 const isAuthenticated = async (req, res, next) => {
   if (req.headers.authorization) {
     const user = await User.findOne({
@@ -7,12 +8,15 @@ const isAuthenticated = async (req, res, next) => {
     });
 
     if (!user) {
+      // User does not exists in DB <=> error 401 <=> Unauthorized
       return res.status(401).json({ error: "Unauthorized" });
     } else {
+      // Creation of the User key in req
       req.user = user;
       return next();
     }
   } else {
+    // error 401 <=> Unauthorized
     return res.status(401).json({ error: "Unauthorized" });
   }
 };
